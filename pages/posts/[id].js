65a1f0b52310ad854782497f5f3569ab42a70fb7
amaisuna <taro30143014@gmail.com>
2021-06-20 02:09:16 +0900

    キャメルケースに変更

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -25,9 +25,9 @@ export default function Post({ post }) {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
             ></path>
           </svg>
